8c099e938103c7ec048e91c2a5f25b4c345bc637
eru123 <yeoligoakino@gmail.com>
2022-02-19 07:50:05 +0800

    update

---
+++ main.js
@@ -0,0 +1,14 @@
+let lastIn = null;
+
+
+
+function switchBox(n) {
+  const in_e = document.getElementById("in");
+  const out_e = document.getElementById("out");
+  if (lastIn) {
+    out_e.className = `btn${lastIn}-out`;
+  }
+
+  lastIn = n;
+  in_e.className = `btn${n}-in`;
+}
